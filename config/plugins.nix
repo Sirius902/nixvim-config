944c406165e84e2497c4f43d1c67189e16bb9ca6
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2024-07-18 01:34:41 -0700

    Add LSP onAttach

--- config/plugins.nix
+++ config/plugins.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   plugins = {
@@ -11,50 +11,109 @@
 
     lsp = {
       enable = true;
-      servers = {
-        # Average webdev LSPs
-        tsserver.enable = true; # TS/JS
-        cssls.enable = true; # CSS
-        html.enable = true; # HTML
-        astro.enable = true; # AstroJS
+      servers =
+        let
+          servers = [
+            # Average webdev LSPs
+            "tsserver"
+            "cssls"
+            "html"
+            "astro"
 
-        # Python
-        pyright.enable = true;
+            # Python
+            "pyright"
 
-        # Markdown
-        marksman.enable = true;
+            # Markdown
+            "marksman"
 
-        # Nix
-        nil-ls.enable = true;
+            # Nix
+            "nil-ls"
 
-        # Docker
-        dockerls.enable = true;
+            # Docker
+            "dockerls"
 
-        # Bash
-        bashls.enable = true;
+            # Bash
+            "bashls"
 
-        # C/C++
-        clangd.enable = true;
+            # C/C++
+            "clangd"
 
-        # C#
-        csharp-ls.enable = true;
+            # C#
+            "csharp-ls"
 
-        # Lua
-        lua-ls = {
-          enable = true;
-          settings.telemetry.enable = false;
-        };
+            # Lua
+            "lua-ls"
 
-        # Rust
-        rust-analyzer = {
-          enable = true;
-          installRustc = false;
-          installCargo = false;
-        };
+            # Rust
+            "rust-analyzer"
 
-        # Zig
-        zls.enable = true;
-      };
+            # Zig
+            "zls"
+          ];
+          sharedConfigs = builtins.map
+            (server: {
+              ${server} = {
+                enable = true;
+                onAttach.override = true;
+                onAttach.function = ''
+                  -- [[ Configure LSP ]]
+                  --  This function gets run when an LSP connects to a particular buffer.
+
+                  -- NOTE: Remember that lua is a real programming language, and as such it is possible
+                  -- to define small helper and utility functions so you don't have to repeat yourself
+                  -- many times.
+                  --
+                  -- In this case, we create a function that lets us more easily define mappings specific
+                  -- for LSP related items. It sets the mode, buffer and description for us each time.
+                  local nmap = function(keys, func, desc)
+                    if desc then
+                      desc = 'LSP: ' .. desc
+                    end
+
+                    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+                  end
+
+                  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                  nmap('<leader>ca', function()
+                    vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
+                  end, '[C]ode [A]ction')
+
+                  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+                  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+                  -- See `:help K` for why this keymap
+                  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+                  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+                  -- Lesser used LSP functionality
+                  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+                  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+                  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+                  nmap('<leader>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                  end, '[W]orkspace [L]ist Folders')
+
+                  -- Create a command `:Format` local to the LSP buffer
+                  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+                    vim.lsp.buf.format()
+                  end, { desc = 'Format current buffer with LSP' })
+                '';
+              };
+            })
+            servers;
+        in
+        lib.mkMerge (sharedConfigs ++ [{
+          lua-ls.settings.telemetry.enable = false;
+
+          rust-analyzer = {
+            installRustc = false;
+            installCargo = false;
+          };
+        }]);
     };
 
     cmp.enable = true;
